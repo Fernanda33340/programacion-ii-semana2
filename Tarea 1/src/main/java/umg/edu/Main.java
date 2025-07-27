05db6fb177c89e8705c5acc8faf0db6026723cc8
Fernanda33340 <mmejiaf2@miumg.edu.gt>
2025-07-27 11:22:42 -0600

    Update Main.java

--- Tarea 1/src/main/java/umg/edu/Main.java
+++ Tarea 1/src/main/java/umg/edu/Main.java
@@ -1,17 +1,15 @@
-package umg.edu;
+programacion-ii-semana2
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Scanner; // Importamos la clase Scanner para leer la entrada del usuario
+public class Semana2_edad { // La clase debe ser pública y el nombre del archivo debe coincidir con el nombre de la clase
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in); // Creamos un objeto Scanner llamado "scanner" para capturar la entrada del usuario
+        System.out.print("Por favor, introduce tu nombre: ");  // Pedimos al usuario que introduzca su nombre
+        String nombre = scanner.nextLine(); // Leemos la entrada del usuario y la guardamos en la variable "nombre"
+        System.out.print("Por favor, introduce tu año de nacimiento: "); // Pedimos al usuario que introduzca su año de nacimiento
+        int añoNacimiento = scanner.nextInt(); // Leemos la entrada del usuario y la guardamos en la variable "añoNacimiento"
+        int añoActual = 2025; // Definimos el ano actual
+        int edad = añoActual - añoNacimiento; // Calculamos la edad del usuario restando su año de nacimiento del año actual
+        System.out.println("Hola" + nombre +", tu edad aproximada es: " + edad + " años."); // Imprimimos un mensaje en la consola con el nombre del usuario y su edad aproximada
     }
 }
